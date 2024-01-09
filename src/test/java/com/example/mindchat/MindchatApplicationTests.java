442631f6d01ba4776f303935c7bb6edfd1a64b2b
simdev1234 <simdev1234@gmail.com>
2024-01-10 01:09:10 +0900

    build : 초기 환경 구성

---
+++ src/test/java/com/example/mindchat/MindchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
